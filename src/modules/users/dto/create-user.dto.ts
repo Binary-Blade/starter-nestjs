34e802c775f3c6e6c5825dcc49bd72b182cb7f10
Binary-Blade <codegrunt@proton.me>
2024-03-20 08:39:54 +0100

    * feat: Add email and password inside entity for typeorm, add class-validator and comment todo later for decorator IsStrongPassword to production mode

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNotEmpty()
+  //TODO: Add that for production : @IsStrongPassword()
+  readonly password: string;
+}
