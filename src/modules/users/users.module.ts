d3de82d6c5d078320ec10d968f5a71fdfd898b94
Binary-Blade <codegrunt@proton.me>
2024-03-20 09:02:49 +0100

    * fix: change routing security.service.ts inside users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,8 +3,8 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { SecurityService } from '../auth/security.service';
 import { JwtService } from '@nestjs/jwt';
+import { SecurityService } from 'src/common/security/security.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
