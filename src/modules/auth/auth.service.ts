5822f2d588562c6e338cc4d13b62bff6c213c09e
Binary-Blade <codegrunt@proton.me>
2024-03-21 08:42:17 +0100

    * feat: Implement login method that returns a token and refreshToken

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,38 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-
+import { Repository } from 'typeorm';
+import { JWTTokens } from 'src/common/interfaces/jwt.interface';
+import { SecurityService } from 'src/core/securities/security.service';
+import { InvalidCredentialsException } from 'src/common/exceptions/invalid-credentials.exception';
+import { TokenService } from 'src/core/securities/token.service';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
     private usersService: UsersService,
+    private securityService: SecurityService,
+    private tokenService: TokenService,
   ) { }
 
   async signup(createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
+
+  async login(email: string, password: string): Promise<JWTTokens> {
+    const user = await this.usersRepository.findOneBy({ email });
+    const validPassword = await this.securityService.verifyPassword(user.password, password);
+
+    if (!user) throw new InvalidCredentialsException();
+    if (!validPassword) throw new InvalidCredentialsException();
+
+    return this.tokenService.getTokens(user);
+  }
+
 }
 
 
