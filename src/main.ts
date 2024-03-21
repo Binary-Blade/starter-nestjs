558d1bb2715b14e9257fd58c61c36fe5f2dec970
Binary-Blade <codegrunt@proton.me>
2024-03-21 08:41:47 +0100

    * feat: Add ValidationPipe, helmet, and implement HttpExceptionFilter to the boostrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { HttpExceptionFilter } from './common/globals-filter/http-exceptions-filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(
+    new HttpExceptionFilter(configService),
+  )
+  // NOTE:: Uncomment that line below to implement the migration-runner auto when starting app
+  // await runMigrations();
   await app.listen(3000);
 }
 bootstrap();
