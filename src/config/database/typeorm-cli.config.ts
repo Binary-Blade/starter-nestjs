715e85bb80dd6b4c90c621c35706995e3bd1cdd6
Binary-Blade <codegrunt@proton.me>
2024-03-22 06:40:49 +0100

    * refactor: Refactoring structure file from core to config

--- src/config/database/typeorm-cli.config.ts
+++ src/config/database/typeorm-cli.config.ts
@@ -5,11 +5,10 @@ const configService = new ConfigService();
 
 export default new DataSource({
 	type: 'postgres',
-	host: configService.get<string>('DB_HOST'),
-	port: configService.get<number>('DB_PORT'),
+	host: 'postgres',
 	database: configService.get<string>('DB_NAME'),
 	username: configService.get<string>('DB_USERNAME'),
 	password: configService.get<string>('DB_PASSWORD'),
-	entities: [], // Path entities
-	migrations: [],
+	entities: [__dirname + '/**/*.entity{.ts,.js}'],
+	migrations: [__dirname + '/**/migrations/*{.ts,.js}'],
 });
---
+++ src/config/migrations/1711085051379-InitialMigration.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1711085051379 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+                CREATE TABLE "users" (
+                    "userId" SERIAL PRIMARY KEY,
+                    "email" VARCHAR NOT NULL,
+                    "password" VARCHAR NOT NULL,
+                    "userRole" VARCHAR NOT NULL
+                );
+            `);
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
