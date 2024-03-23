825ffbccf96783bae6736388dacd3d561d4fcf8c
Binary-Blade <codegrunt@proton.me>
2024-03-23 05:39:30 +0100

    * feat: Add role guard for user role  admin/user

---
+++ src/common/guards/role.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { User } from "src/modules/users/entities/user.entity";
+import { ROLE_KEY } from "../decorators/role.decorator";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+	constructor(private reflector: Reflector) { }
+
+	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+		const requiredRole = this.reflector.getAllAndOverride<User>(
+			ROLE_KEY, [context.getHandler(), context.getClass()]
+		)
+		if (!requiredRole) {
+			return true;
+		}
+		const { user } = context.switchToHttp().getRequest();
+		return user.role === requiredRole;
+	}
+}
