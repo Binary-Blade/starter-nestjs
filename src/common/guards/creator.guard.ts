9c93a3a699cf63dd0415495b3fa1584c601eb44f
Binary-Blade <codegrunt@proton.me>
2024-03-23 06:00:47 +0100

    * feat: Create creator.guard to user can see only that own content for better and concis code

---
+++ src/common/guards/creator.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable, CanActivate, ExecutionContext, NotFoundException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class CreatorGuard implements CanActivate {
+	constructor(private reflector: Reflector) { }
+
+	canActivate(context: ExecutionContext): boolean {
+		const request = context.switchToHttp().getRequest();
+		const user = request.user;
+		const contentOwnerId = +request.params.id;
+
+		const isCreator = user && user.userId === contentOwnerId;
+		if (!isCreator) {
+			throw new NotFoundException('User not found');
+		}
+
+		request.isCreator = isCreator;
+		return true;
+	}
+}
+
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,34 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AccessTokenGuard } from 'src/common/guards/access-token.guard';
+import { Role } from 'src/common/decorators/role.decorator';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { UserRole } from 'src/common/enums/user-role.enum';
+import { CreatorGuard } from 'src/common/guards/creator.guard';
+
 
 @UseGuards(AccessTokenGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
-  @Post('create')
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
+  // HACK: Maybe create a service and controller admin for Dashboard modules ?
+  @Role(UserRole.ADMIN)
+  @UseGuards(RoleGuard)
   @Get('getAll')
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(CreatorGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @UseGuards(CreatorGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @UseGuards(CreatorGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
