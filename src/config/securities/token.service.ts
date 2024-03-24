07800b35e4edb8202198a3b920eafa25b909d446
Binary-Blade <codegrunt@proton.me>
2024-03-24 04:48:53 +0100

    * refactor: Refactoring methods to be more organize and add comments

--- src/config/securities/token.service.ts
+++ src/config/securities/token.service.ts
@@ -6,47 +6,69 @@ import { JWTTokens } from "@common/interfaces/jwt.interface";
 import { User } from "@modules/users/entities/user.entity";
 import { Repository } from "typeorm";
 
+/**
+ * Service responsible for managing JWT tokens, including their creation and validation.
+ * This service utilizes the JwtService for signing tokens with specified secrets and expiration times.
+ */
 @Injectable()
 export class TokenService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
-    private jwtService: JwtService,
-    private configService: ConfigService,
-
+    private usersRepository: Repository<User>, // Injects the User repository for database interactions.
+    private jwtService: JwtService, // Injects Nest's JwtService for token operations.
+    private configService: ConfigService, // Injects ConfigService for accessing environment variables.
   ) { }
 
-  // TODO: Create one unique token per user 
+  /**
+   * Generates JWT access and refresh tokens for a user.
+   * 
+   * @param user The user entity for whom the tokens are being generated.
+   * @returns A promise that resolves to an object containing the generated tokens.
+   */
   async getTokens(user: User): Promise<JWTTokens> {
-    const [token, refreshToken] = await Promise.all([
-      this.jwtService.signAsync(
-        { sub: user.userId, role: user.role },
-        {
-          secret: this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
-          expiresIn: this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRATION'),
-        }
-      ),
-      this.jwtService.signAsync(
-        { sub: user.userId, role: user.role },
-        {
-          secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
-          expiresIn: this.configService.get<string>('JWT_REFRESH_TOKEN_EXPIRATION'),
-        }
-      ),
-    ]);
-    return { token, refreshToken }
+    const payload = {
+      sub: user.userId, // Subject field typically contains the user identifier.
+      role: user.role, // Custom claim to include the user's role in the token.
+      version: user.tokenVersion, // Includes tokenVersion to support token invalidation.
+    };
+
+    // Generates the access token with a specific secret and expiration time.
+    const token = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRATION'),
+    });
+
+    // Generates the refresh token with a different secret and longer expiration time.
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_REFRESH_TOKEN_EXPIRATION'),
+    });
+
+    return { token, refreshToken };
   }
 
+  /**
+   * Validates a refresh token and generates new JWT tokens if the refresh token is valid.
+   * 
+   * @param token The refresh token to be validated.
+   * @returns A promise that resolves to new JWT access and refresh tokens.
+   * @throws UnauthorizedException If the refresh token is invalid or the user does not exist.
+   */
   async refreshToken(token: string): Promise<JWTTokens> {
     try {
+      // Verifies the refresh token using its secret.
       const { sub: userId } = await this.jwtService.verifyAsync(token, {
         secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
       });
+
+      // Retrieves the user based on the userId from the token's payload.
       const user = await this.usersRepository.findOneOrFail({ where: { userId } });
 
+      // Generates new tokens for the user.
       return this.getTokens(user);
     } catch (err) {
       throw new UnauthorizedException();
     }
   }
 }
+
