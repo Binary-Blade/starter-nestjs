a55352654d3219d239da71f01d21f8edaeea391d
Binary-Blade <codegrunt@proton.me>
2024-04-04 11:35:41 +0200

    * delete: Remove class validator

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,16 +1,13 @@
-import { IsEmail, IsEnum, IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 import { UserRole } from '../enums/user-role.enum';
 
 export class CreateUserDto {
-  @IsEmail()
   @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
 
   @IsNotEmpty()
-
-  //@IsStrongPassword()
   readonly password: string;
 
-  @IsEnum(UserRole)
   readonly role?: UserRole;
 }
