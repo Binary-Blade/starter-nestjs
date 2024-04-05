63fd7ecd7eb049ef6e9d2d44b24b3c91903c03e2
Binary-Blade <codegrunt@proton.me>
2024-04-05 22:24:58 +0200

    * feat: Add redismodule and redisService

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,14 +3,17 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { RedisModule } from '@database/redis/redis.module';
+import { RedisService } from '@database/redis/redis.service';
 import { AccessTokenStrategy } from '@security/auth/strategies/access-token.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // Registers the User entity for TypeORM
+  imports: [TypeOrmModule.forFeature([User]), RedisModule], // Registers the User entity for TypeORM
   controllers: [UsersController], // The controllers that are part of this module
   providers: [
     UsersService, // The service responsible for user-related operations
-    AccessTokenStrategy // AccessTokenStrategy implements JWT validation logic for Passport.
+    AccessTokenStrategy, // AccessTokenStrategy implements JWT validation logic for Passport.
+    RedisService
   ]
 })
 export class UsersModule {}
