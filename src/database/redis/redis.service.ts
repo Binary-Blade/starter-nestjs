771f7475b6c053716a56fa43a3f153d126ea33f8
Binary-Blade <codegrunt@proton.me>
2024-04-06 01:02:38 +0200

    * refactor: Upgrade security type and add logger

--- src/database/redis/redis.service.ts
+++ src/database/redis/redis.service.ts
@@ -1,28 +1,38 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException, Logger } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
+  private readonly logger = new Logger(RedisService.name);
+
   constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
 
-  async set(key: string, value: string, ttl: number): Promise<string> {
-    await this.redisClient.set(key, value, 'EX', ttl);
+  async set(key: string, value: any, ttl?: number): Promise<string> {
+    const stringValue = typeof value === 'string' ? value : JSON.stringify(value);
+    if (typeof ttl === 'number') {
+      await this.redisClient.set(key, stringValue, 'EX', ttl);
+    } else {
+      await this.redisClient.set(key, stringValue);
+    }
     return `Data set for key: ${key}`;
   }
 
   async get(key: string): Promise<string | null> {
-    const value = await this.redisClient.get(key);
-    if (value) {
-      console.log(`Retrieved value from cache: ${value}`);
-    } else {
-      console.log(`No value found in cache for key: ${key}`);
+    try {
+      const value = await this.redisClient.get(key);
+      if (!value) return null;
+      return value;
+    } catch (error) {
+      this.logger.error(`Error retrieving key ${key} from cache`, error);
+      throw error;
     }
-    return value;
   }
 
   async del(key: string): Promise<number> {
     const result = await this.redisClient.del(key);
-    console.log(`Deleted key from cache: ${key}, result: ${result}`);
-    return result; // Returns the number of keys that were removed.
+    if (!result) {
+      throw new NotFoundException(`Key not found in cache: ${key}`);
+    }
+    return result;
   }
 }
