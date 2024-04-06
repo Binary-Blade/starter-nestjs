230de1649d134db5a3c1e1f1031a510d77d6d84a
Binary-Blade <codegrunt@proton.me>
2024-04-06 18:37:51 +0200

    * refactor: Refactoring to use prefix logic inside the redis
    Use prefix to know when cache prod and dev value is store

--- src/database/redis/redis.service.spec.ts
+++ src/database/redis/redis.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import Redis from 'ioredis';
 import { RedisService } from './redis.service';
 import { NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 jest.mock('ioredis', () => ({
   default: jest.fn().mockImplementation(() => ({
@@ -15,66 +16,84 @@ describe('RedisService', () => {
   let service: RedisService;
   let redisClient: Redis;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        RedisService,
-        {
-          provide: 'REDIS_CLIENT',
-          useClass: Redis
-        }
-      ]
-    }).compile();
+  const environments = [
+    { env: 'development', prefix: 'dev:' },
+    { env: 'production', prefix: 'prod:' }
+  ];
 
-    service = module.get<RedisService>(RedisService);
-    redisClient = module.get('REDIS_CLIENT');
-  });
+  environments.forEach(({ env, prefix }) => {
+    describe(`when environment is ${env}`, () => {
+      beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [
+            RedisService,
+            {
+              provide: 'REDIS_CLIENT',
+              useClass: Redis
+            },
+            {
+              provide: ConfigService,
+              useValue: {
+                get: jest.fn((key: string) => {
+                  if (key === 'NODE_ENV') return env;
+                  return prefix; // Assuming that for simplicity, any other call returns the prefix directly
+                })
+              }
+            }
+          ]
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<RedisService>(RedisService);
+        redisClient = module.get('REDIS_CLIENT');
+      });
 
-  describe('set', () => {
-    it('should call redis.set with the correct parameters', async () => {
-      const key = 'testKey';
-      const value = 'testValue';
-      const ttl = 3600;
-      await service.set(key, value, ttl);
-      expect(redisClient.set).toHaveBeenCalledWith(key, value, 'EX', ttl);
-    });
-  });
+      it('should be defined', () => {
+        expect(service).toBeDefined();
+      });
 
-  describe('get', () => {
-    it('should retrieve the correct value for a key', async () => {
-      const key = 'testKey';
-      const expectedValue = 'testValue';
-      (redisClient.get as jest.Mock).mockResolvedValue(expectedValue);
-      const result = await service.get(key);
-      expect(result).toBe(expectedValue);
-      expect(redisClient.get).toHaveBeenCalledWith(key);
-    });
+      it('should call redis.set with the correct parameters including prefix', async () => {
+        const key = 'testKey';
+        const value = 'testValue'; // Assuming value is already a string based on your test
+        const ttl = 3600;
+        const expectedFullKey = `${prefix}${key}`;
+        await service.set(key, value, ttl);
+        expect(redisClient.set).toHaveBeenCalledWith(expectedFullKey, value, 'EX', ttl);
+      });
 
-    it('should return null when the key does not exist', async () => {
-      const key = 'nonexistentKey';
-      (redisClient.get as jest.Mock).mockResolvedValue(null);
-      const result = await service.get(key);
-      expect(result).toBeNull();
-    });
-  });
+      it('should retrieve the correct value for a key including prefix', async () => {
+        const key = 'testKey';
+        const expectedValue = 'testValue';
+        const expectedFullKey = `${prefix}${key}`;
+        (redisClient.get as jest.Mock).mockResolvedValue(expectedValue);
+        const result = await service.get(key);
+        expect(result).toBe(expectedValue);
+        expect(redisClient.get).toHaveBeenCalledWith(expectedFullKey);
+      });
 
-  describe('del', () => {
-    it('should delete a key', async () => {
-      const key = 'testKey';
-      (redisClient.del as jest.Mock).mockResolvedValue(1);
-      const result = await service.del(key);
-      expect(result).toBe(1);
-      expect(redisClient.del).toHaveBeenCalledWith(key);
-    });
+      it('should return null when the key with prefix does not exist', async () => {
+        const key = 'nonexistentKey';
+        const expectedFullKey = `${prefix}${key}`;
+        (redisClient.get as jest.Mock).mockResolvedValue(null);
+        const result = await service.get(key);
+        expect(result).toBeNull();
+        expect(redisClient.get).toHaveBeenCalledWith(expectedFullKey);
+      });
 
-    it('should throw NotFoundException when trying to delete a nonexistent key', async () => {
-      const key = 'nonexistentKey';
-      (redisClient.del as jest.Mock).mockResolvedValue(0);
-      await expect(service.del(key)).rejects.toThrow(NotFoundException);
+      it('should delete a key including prefix', async () => {
+        const key = 'testKey';
+        const expectedFullKey = `${prefix}${key}`;
+        (redisClient.del as jest.Mock).mockResolvedValue(1);
+        const result = await service.del(key);
+        expect(result).toBe(1);
+        expect(redisClient.del).toHaveBeenCalledWith(expectedFullKey);
+      });
+
+      it('should throw NotFoundException when trying to delete a nonexistent key including prefix', async () => {
+        const key = 'nonexistentKey';
+        const expectedFullKey = `${prefix}${key}`;
+        (redisClient.del as jest.Mock).mockResolvedValue(0);
+        await expect(service.del(key)).rejects.toThrow(NotFoundException);
+      });
     });
   });
 });
--- src/database/redis/redis.service.ts
+++ src/database/redis/redis.service.ts
@@ -1,37 +1,54 @@
+import { DEV_PREFIX, NODE_ENV, PROD_ENV, PROD_PREFIX } from '@common/constants';
 import { Injectable, Inject, NotFoundException, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
   private readonly logger = new Logger(RedisService.name);
+  private readonly keyPrefix: string;
 
-  constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
+  constructor(
+    @Inject('REDIS_CLIENT') private readonly redisClient: Redis,
+    @Inject(ConfigService) private readonly configService: ConfigService
+  ) {
+    this.keyPrefix =
+      this.configService.get<string>(NODE_ENV) === PROD_ENV ? PROD_PREFIX : DEV_PREFIX;
+  }
+
+  private formatKey(key: string): string {
+    return `${this.keyPrefix}${key}`;
+  }
 
   async set(key: string, value: any, ttl?: number): Promise<string> {
+    const fullKey = this.formatKey(key);
     const stringValue = typeof value === 'string' ? value : JSON.stringify(value);
     if (typeof ttl === 'number') {
-      await this.redisClient.set(key, stringValue, 'EX', ttl);
+      await this.redisClient.set(fullKey, stringValue, 'EX', ttl);
     } else {
-      await this.redisClient.set(key, stringValue);
+      await this.redisClient.set(fullKey, stringValue);
     }
-    return `Data set for key: ${key}`;
+    return `Data set for key: ${fullKey}`;
   }
 
   async get(key: string): Promise<string | null> {
+    const fullKey = this.formatKey(key);
     try {
-      const value = await this.redisClient.get(key);
+      const value = await this.redisClient.get(fullKey);
       if (!value) return null;
       return value;
     } catch (error) {
-      this.logger.error(`Error retrieving key ${key} from cache`, error);
+      this.logger.error(`Error retrieving key ${fullKey} from cache`, error);
       throw error;
     }
   }
 
   async del(key: string): Promise<number> {
-    const result = await this.redisClient.del(key);
+    const fullKey = this.formatKey(key);
+    const result = await this.redisClient.del(fullKey);
     if (!result) {
-      throw new NotFoundException(`Key not found in cache: ${key}`);
+      this.logger.warn(`Key not found in cache: ${fullKey}`);
+      throw new NotFoundException(`Key not found in cache: ${fullKey}`);
     }
     return result;
   }
